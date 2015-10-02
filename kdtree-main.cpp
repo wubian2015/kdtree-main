56dab3ef8bb5dfdc351b78a3481551d84fecddfa
wubian <wb-mac@wubiandeMacBook-Air.local>
2015-10-02 16:24:55 +0800

    add the main.cpp

---
+++ kdtree-main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : kdtree-main.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hi wu bian" << endl; // prints !!!Hello World!!!
+	return 0;
+}
